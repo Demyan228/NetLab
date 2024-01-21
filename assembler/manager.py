be2dbb223702b7b608c9e1a499d70128ad5cffbb
bon-clay <bon.clay.4000@gmail.com>
2024-01-21 12:57:37 +0500

    version 1 dev

--- assembler/manager.py
+++ assembler/manager.py
@@ -1,11 +1,12 @@
 from base_manager import BaseManager
+from my_tools import log
 
 
 class AssemblerManager(BaseManager):
 
     def __init__(self):
         super().__init__()
-        print('ASSEMBLER MANAGER INIT')
+        log('ASSEMBLER MANAGER INIT')
 
 
 manager = AssemblerManager()
